a0023eb0ccd77760be16a02377ab126405c50e0d
bkysyoon <bkysyoon@gmail.com>
2026-04-26 11:09:46 -0700

    Refactor game logic and update pause time
    Updated pauseTime variable and added game logic functions for handling correct and wrong selections.

--- test.js
+++ test.js
@@ -1,6 +1,13 @@
-var pauseTime=1.9;
+var pauseTime=3;
+var selectedSafe = false
+var selectedOut = false
+const a = ["s", "o", "o", "s", "o", "o", "s", "o", "o", "s", "s"];
+var index = 0;
 var myVideo=document.getElementById("vid");
 var qOverlay=document.getElementById("questions");
+var rightOverlay =document.getElementById("rOverlay");
+var wrongOverlay =document.getElementById("wOverlay");
+var LoseOverlay =document.getElementById("gOverlay");
 myVideo.defaultPlaybackRate = 1;
 myVideo.load();
 myVideo.addEventListener('timeupdate', function(){
@@ -10,10 +17,61 @@ myVideo.addEventListener('timeupdate', function(){
     }
 });
 
-function q(){
-    qOverlay.style.display="none";
+function isCorrect() {
+    if (a[index] === "s" && selectedSafe) {
+        index++;
+        return true;
+    }
+    else if (a[index] === "o" && selectedOut) {
+        index++;
+        return true;
+    }
+    index++;
+    return false;
+}
+
+function q() {
+    qOverlay.style.display = "none";
 }
 
 
+function gameOver() {
+    LoseOverlay.style.display = "block";
+}
 
 
+function safe() {
+    selectedSafe = true;
+    if (isCorrect()) {
+        right();
+    }
+    else {
+        wrong();
+    }
+}
+
+
+function out() {
+    selectedOut = true;
+    if (isCorrect()) {
+        right();
+    }
+    else {
+        wrong();
+    }
+}
+
+
+function right() {
+    rightOverlay.style.display = "block";
+    selectedSafe = false;
+    selectedOut = false;
+}
+
+
+function wrong() {
+    q()
+    wrongOverlay.style.display = "block";
+    selectedSafe = false;
+    selectedOut = false;
+}
